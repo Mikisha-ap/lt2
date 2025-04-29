4ec03d15d1f04befc536e954f1caa256092a908a
Mikisha Pillay <2426761@students.wits.ac.za>
2025-04-29 17:03:55 +0200

    Adding first test

--- tests.cpp
+++ tests.cpp
@@ -1,1 +1,22 @@
+#include "bmi.cpp"
+#include <iostream>
 
+//Checks if this method is functioning properly and converts everything correctly
+void testWeightToKG()
+{
+    if (weight2kg(0,0,0) == 0 && weight2kg(1,0,0) > 6.3 && weight2kg(1,0,0) < 6.4){
+        std::cout << "WeightToKg test passed." <<std::endl;
+    }
+    else
+    {
+        std::cout << "WeightToKg test failed." <<std::endl;
+    }
+}
+
+
+
+
+int dev(int argc, char *argv[]){
+    testWeightToKG();    
+    return 0;
+}
