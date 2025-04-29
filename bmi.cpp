e3f78f4d4fcf253f7b2e392a66ea895c45b07c9b
Mikisha Pillay <2426761@students.wits.ac.za>
2025-04-29 16:08:33 +0200

    Correcting the bmi.cpp programme

--- bmi.cpp
+++ bmi.cpp
@@ -6,11 +6,14 @@
 
 using namespace std;
 
+// Converts ounces into pounds
 int ounces2pounds(int x)
 {
-    return(x*16);
+    return(x/16);
+    //Corrected calculation
 }
 
+//Converts stones into pounds
 int stones2pounds(int x)
 {
     return(x*14);
@@ -23,12 +26,13 @@ double weight2kg(int stones, int pounds, int ounces)
 
 double height2metres(int feet, int inches)
 {
-    return(feet/3.82);
+    //Corrected calculation 
+    return(feet * 12 + inches) * 0.0254;
 }
 
 char categorise(double kg, double metre)
 {
-    double bmi = kg*kg/metre;
+    double bmi = kg/(metre*metre);
     char cat;
     if (bmi<19)
         cat='A';
@@ -67,6 +71,11 @@ void process_data(char* input_file, char* output_file)
         
 int main(int argc, char *argv[])
 {
+    // Checking if 3 arguments were supplied
+    if(argc != 3){
+        return 1;
+    }
     // KJN - Need to check that 3 arguments were supplied upon execution
     process_data(argv[1], argv[2]);
+    return 0;
 }
